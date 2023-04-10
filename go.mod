8439d4b3430b0d691cfbd25e8c41b73f34f67a56
Sumit Prasad <prasadsumit99544@gmail.com>
2023-04-11 01:45:39 +0530

    Updated main.go and user.go

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/sumit-prasad/gitgo
 
 go 1.20
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/ogier/pflag v0.0.1 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "strings"
+    flag "github.com/ogier/pflag"
+    color "github.com/fatih/color"
+)
+
+
+var (
+    user string
+)
 
 func main() {
-    fmt.Println("Hello World")
+    // parse the flag
+    flag.Parse()
+
+    // when no flag is specified
+    if flag.NFlag() == 0 {
+        fmt.Printf("Usage: %s [options]\n", os.Args[0])
+        fmt.Println("Options")
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
+
+    // split the multiple users into individual name
+    users := strings.Split(user, ",")
+
+    // Print users to the screen
+    fmt.Printf("Searching user(s): %s\n", users)
+    for _, u := range users {
+        result := getUsers(u)
+        color.Cyan(`Username:  %s`, result.Login)
+        color.Blue(`Name:      %s`, result.Name)
+        color.Green(`Email:        %s`, result.Email)
+        color.HiMagenta(`Bio:      %s`, result.Bio)
+        color.Cyan(`Location:   %s`, result.Location)
+        color.Green(`Created On:   %s`, result.CreatedAt)
+    }
+}
+
+func init() {
+    flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
 
